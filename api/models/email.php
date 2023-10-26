fb063dcb622a68aed1634992a6229cbb857c2d57
coderoycc <rcchambi4@gmail.com>
2023-10-26 13:20:23 -0400

    Email

--- api/models/email.php
+++ api/models/email.php
@@ -1,56 +1,56 @@
 <?php
-// require './vendor/PHPMailer-master/src/PHPMailer.php';
-// require './vendor/PHPMailer-master/src/SMTP.php';
-// require './vendor/PHPMailer-master/src/Exception.php';
-// use PHPMailer\PHPMailer\PHPMailer;
-// class Email{
-//   private $to;
-//   private $subject = 'Codigo de verificación';
-//   private $body = '<h4>Envio del codigo de verificación</h4><hr>El código es:';
+require './vendor/PHPMailer-master/src/PHPMailer.php';
+require './vendor/PHPMailer-master/src/SMTP.php';
+require './vendor/PHPMailer-master/src/Exception.php';
+use PHPMailer\PHPMailer\PHPMailer;
+class Email{
+  private $to;
+  private $subject = 'Codigo de verificación';
+  private $body = '<h4>Envio del codigo de verificación</h4><hr>El código es:';
 
-//   private static $host = 'smtp.outlook.com';
-//   private static $port = 587;
-//   private static $smtpSecure = 'tls';
+  private static $host = 'smtp.outlook.com';
+  private static $port = 587;
+  private static $smtpSecure = 'tls';
 
-//   private static $from = 'stisbo@outlook.com';
+  private static $from = 'stisbo@outlook.com';
 
-//   private static $password = 'BOstis123*';
+  private static $password = 'BOstis123*';
 
-//   public function __construct($to, $bodyCode){
-//     $this->to = $to;
-//     $this->body = $this->body.'<b> '.$bodyCode.'</b>';
-//   }
+  public function __construct($to, $bodyCode){
+    $this->to = $to;
+    $this->body = $this->body.'<b> '.$bodyCode.'</b>';
+  }
 
-//   public function send(){
-//     try{
-//       $mail = new PHPMailer();
-//       $mail->SMTPDebug = 0;  
-//       $mail->CharSet = "UTF-8";
-//       $mail->isSMTP();
-//       $mail->Host = self::$host;
-//       $mail->Port = self::$port;
-//       $mail->SMTPSecure = self::$smtpSecure;
-//       $mail->SMTPAuth = true;
+  public function send(){
+    try{
+      $mail = new PHPMailer();
+      $mail->SMTPDebug = 0;  
+      $mail->CharSet = "UTF-8";
+      $mail->isSMTP();
+      $mail->Host = self::$host;
+      $mail->Port = self::$port;
+      $mail->SMTPSecure = self::$smtpSecure;
+      $mail->SMTPAuth = true;
 
-//       $mail->Username = self::$from;
-//       $mail->Password = self::$password;
-//       $mail->setFrom(self::$from);
-//       $mail->addAddress($this->to);
-//       $mail->Subject = $this->subject;
+      $mail->Username = self::$from;
+      $mail->Password = self::$password;
+      $mail->setFrom(self::$from);
+      $mail->addAddress($this->to);
+      $mail->Subject = $this->subject;
 
-//       $mail->isHTML(true);
-//       $mail->Body = $this->body;
-//       if($mail->send()){
-//         return true;
-//       }else{
-//         return false;
-//       }
-//     }catch(Exception $e){
-//       print_r($e);
-//     }
-//   }
+      $mail->isHTML(true);
+      $mail->Body = $this->body;
+      if($mail->send()){
+        return true;
+      }else{
+        return false;
+      }
+    }catch(Exception $e){
+      print_r($e);
+    }
+  }
 
   
 
-// }
+}
 ?>
